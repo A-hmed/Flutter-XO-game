af4c8d4ad5ce079b23b3c49fd3ead633b9e0ce69
Ahmed Nabil <an0088039@gmail.com>
2022-03-04 11:21:49 +0200

    removed title in MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:session2_flutter_c5_alex/GameBoardScreen.dart';
 import 'package:session2_flutter_c5_alex/GameHome.dart';
 
-import 'Calculator.dart';
-
 void main() {
   runApp(MyApp());
 }
@@ -13,7 +11,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       routes: {
         GameBoardScreen.ROUTE_NAME:(context)=>GameBoardScreen(),
         GameHome.ROUTE_NAME:(context)=>GameHome()
